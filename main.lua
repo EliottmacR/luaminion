2a743692a6b83c5c5c3307fc4fcb025911189db4
Eliott <eliotts@protonmail.com>
2019-05-06 09:43:16 +0200

    typo in main

--- main.lua
+++ main.lua
@@ -19,11 +19,11 @@ function love.draw()
     local height = love.graphics.getHeight()
         
     -- sets zoom so game sticks to either height or width
-    zoom = height/(GH+  bordure)
-    if (width < (GW + bordure) * zoom) then zoom = width / (GW + bordure) end
+    zoom = height/(GH+  border)
+    if (width < (GW + border) * zoom) then zoom = width / (GW + border) end
       
     -- centers screen
-    love.graphics.translate(0.5 * (width - (GW + bordure/2 ) * zoom),  0.5 * (height - (GH + bordure/2) * zoom))
+    love.graphics.translate(0.5 * (width - (GW + border/2 ) * zoom),  0.5 * (height - (GH + border/2) * zoom))
     love.graphics.scale(zoom)  
     
     -- after this point, everything is in 800 x 600
@@ -46,8 +46,8 @@ function get_mouse_pos()
   
   local width  = love.graphics.getWidth()
   local height = love.graphics.getHeight()
-  local x = (truex - (width -  (GW  + bordure/2) * zoom) / 2) / zoom
-  local y = (truey - (height - (GH + bordure/2) * zoom) / 2) / zoom
+  local x = (truex - (width -  (GW  + border/2) * zoom) / 2) / zoom
+  local y = (truey - (height - (GH + border/2) * zoom) / 2) / zoom
   
   return x, y
 
